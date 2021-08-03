004eed7d2151d3905ea20ab9c6e835a3894ed9f2
PavelZubkov <zubkov_p@mail.ru>
2021-08-03 22:23:11 +0300

    $aspirity_restmock: Set pages width

---
+++ restmock.view.css.ts
@@ -0,0 +1,27 @@
+namespace $.$$ {
+
+	const { rem } = $mol_style_unit
+
+	$mol_style_define( $aspirity_restmock , {
+
+		List_page: {
+
+			flex: {
+				basis: rem(32),
+				shrink: 0,
+			},
+
+		},
+		
+		Details_page: {
+
+			flex: {
+				grow: 1000,
+				basis: rem(42),
+			},
+
+		},
+
+	} )
+
+}
